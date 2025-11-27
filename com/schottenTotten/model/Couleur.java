25637f2d6bc9a4a1ac814ce076bde8cac31a7257
Thomas RENAULT <Thomas.Renault@bordeaux-inp.fr>
2025-11-27 12:21:12 +0100

    ajout de la classe Couleur

--- com/schottenTotten/model/Couleur.java
+++ com/schottenTotten/model/Couleur.java
@@ -1,3 +1,44 @@
 public class Couleur{
+    private int r;
+    private int g;
+    private int b;
 
+    public Couleur(int r, int g, int b){
+        this.r = couleurValide(r);
+        this.g = couleurValide(g);
+        this.b = couleurValide(b);
+    }
+
+    public Couleur(){
+        this(0,0,0);
+    }
+
+    private int couleurValide(int couleur){
+        if (couleur < 0) {
+            return 0;
+        } else if (couleur > 255) {
+            return 255;
+        } else {
+            return couleur;
+        }
+    }
+
+    public static Couleur bleu(){
+        return new Couleur(0,0,255);
+    }
+    public static Couleur vert(){
+        return new Couleur(0,255,0);
+    }
+    public static Couleur rouge(){
+        return new Couleur(255,0,0);
+    }
+    public static Couleur jaune(){
+        return new Couleur(255,255,0);
+    }
+    public static Couleur violet(){
+        return new Couleur(127,0,255);
+    }
+    public static Couleur marron(){
+        return new Couleur(210,180,140);
+    }
 }
